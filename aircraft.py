37659d92e85d1013fe70a9662c179b02415cee10
rafaelimai <rafael@DESKTOP-RPTV4A0>
2024-10-16 19:41:54 -0300

    developing further the aircraft model

--- aircraft.py
+++ aircraft.py
@@ -11,18 +11,38 @@
 # Project: Distributed Control and Stochastic Analysis of Hybrid Systems Supporting
 # Safety Critical Real-Time Systems Design (HYBRIDGE)
 
+INDEX_MAX_SPEED = 0
+INDEX_ACCEL_DECEL_RATE = 1
+INDEX_TURNING_RATE = 2
+INDEX_RATE_OF_CLIMB = 3
+INDEX_RATE_OF_DESCENT = 4
+INDEX_FUEL_BURN_QUADRATIC = 5
+INDEX_FUEL_BURN_LINEAR = 6
+INDEX_FUEL_BURN_BASE = 7
 
+class Aircraft: 
 
-class Aircraft:
-    INDEX_MAX_SPEED = 0
-    INDEX_ACCEL_DECEL_RATE = 1
-    INDEX_TURNING_RATE = 2
-    INDEX_RATE_OF_CLIMB = 3
-    INDEX_RATE_OF_DESCENT = 4
-    INDEX_FUEL_BURN_QUADRATIC = 5
-    INDEX_FUEL_BURN_LINEAR = 6
-    INDEX_FUEL_BURN_BASE = 7
+    current_time = 0
+    fuel_level = 0
+    x, y, z = 0, 0, 0
+    route = []
 
+    def __init__(self, params, fuel_level, x, y, z):
+        self.max_speed = params[INDEX_MAX_SPEED]
+        self.accel_rate = params[INDEX_ACCEL_DECEL_RATE]
+        self.turning_rate = params[INDEX_TURNING_RATE]
+        self.roc = params[INDEX_RATE_OF_CLIMB]
+        self.rod = params[INDEX_RATE_OF_DESCENT]
+        self.fuel_burn_quad = params[INDEX_FUEL_BURN_QUADRATIC]
+        self.fuel_burn_linear = params[INDEX_FUEL_BURN_LINEAR]
+        self.fuel_burn_base = params[INDEX_FUEL_BURN_BASE] 
+        self.fuel_level = fuel_level
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def advance_step(delta_time):
+        print("todo")
 
-    def __init__(self, params):
-        
+    def check_distance(aircraft, threshold):
+        print("todo")
